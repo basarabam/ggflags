bc59bf91e969cb0a21b517a00487ab7f867779d6
Milos Basaraba <milos.basaraba@gmail.com>
2019-05-02 10:19:44 +0200

    Refreshed pdf list of flags
    Updated demo flags pdf and changed location of savig pdf of all flags

--- inst/demo/plot_flags.R
+++ inst/demo/plot_flags.R
@@ -7,4 +7,4 @@ lg <- lapply(.flaglist, rasterGrob, height=unit(5,"mm"))
 
 gl <- mapply(function(x,y) arrangeGrob(x, top=y), x=lg, y=lf, SIMPLIFY = FALSE)
 
-ggsave("flags.pdf", grid.arrange(grobs=gl), width=10, height=10)
+ggsave("inst/demo/flags.pdf", grid.arrange(grobs=gl), width=10, height=10)
