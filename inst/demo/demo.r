61e943844b079059e19a122e34e3eabd064d0442
Milos Basaraba <milos.basaraba@gmail.com>
2019-05-02 10:09:06 +0200

    Small changes in package
    Updated some flags
    Changed way of updating flags, added resize function and r cleaning data folder with png1 folder
    Package added to dependancies to avoid errors with grob
    Function scale discrete adjusted to new version of  it in ggflags

--- inst/demo/demo.r
+++ inst/demo/demo.r
@@ -3,9 +3,8 @@ library(ggflags)
 data(lflags)
 set.seed(1234)
 d <- data.frame(x=rnorm(10), y=rnorm(10), 
-                country=sample(c("ar","fr"), 10, TRUE), 
+                country=sample(c("yu","fr"), 50, TRUE), 
                 stringsAsFactors = FALSE)
 ggplot(d, aes(x=x, y=y, country=country, size=x)) + 
-  geom_flag() + 
+  geom_flag() +
   scale_country()
-
--- inst/store_flags.R
+++ inst/store_flags.R
@@ -1,13 +1,22 @@
-library(png)
 
+library(magick)
 
-# mogrify -resize 10%  -format png -quality 75 -path ../flags *.png
+# Reading stored png images in png1 folder (store folder with
+# png images in folder png1)
 
-lf <- list.files(path="png", pattern="\\.png$", full.names = TRUE)
+lf1 <- list.files(path="png1", pattern="\\.png$", full.names = TRUE)
+images <- lapply(lf1, image_read)
 
+# resize images to 100X100 it is not reccomended to be larger
+# for more items use larger resize for example 50 x 50
+image_rszd <- lapply(images, image_resize, "100x100")
 
-.flaglist <- lapply(lf, readPNG)
-names(.flaglist) <- tolower(gsub("^png/|\\.png", "", lf))
+#converts images to raster, and names it
+.flaglist <- lapply(image_rszd, as.raster)
+names(.flaglist) <- tolower(gsub("^png1/|\\.png", "", lf1))
 
-save(.flaglist, file="lflags.rda")
-
+#saving and removing files not needed
+usethis::use_data(.flaglist, overwrite = TRUE, compress = TRUE)
+file.remove("data/lflags.rda")
+file.rename("data/.flaglist.rda", "data/lflags.rda")
+unlink("png1", recursive=TRUE)
