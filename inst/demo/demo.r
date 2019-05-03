38fff8eade21658e78aa336507e16cb03df55842
Milos Basaraba <milos.basaraba@gmail.com>
2019-05-03 17:16:47 +0200

    Updated flag
    Updated missing flag for historical data - Germany DR

--- inst/demo/demo.r
+++ inst/demo/demo.r
@@ -1,9 +1,9 @@
 library(ggflags)
 
 data(lflags)
-set.seed(1234)
+set.seed(1235)
 d <- data.frame(x=rnorm(10), y=rnorm(10), 
-                country=sample(c("yu", "fr", "gb"), 50, TRUE), 
+                country=sample(c("yu", "dr-de", "gb"), 50, TRUE), 
                 stringsAsFactors = FALSE)
 ggplot(d, aes(x=x, y=y, country=country, size=x)) + 
   geom_flag() +
